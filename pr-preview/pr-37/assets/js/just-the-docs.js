7d605698d530f6cee9c2aa62dde228d31a7a662b
ericyuxuanye <ericyuxuanye@users.noreply.github.com>
2024-10-27 06:14:47 +0000

    Deploy preview for PR 37 🛫

--- pr-preview/pr-37/assets/js/just-the-docs.js
+++ pr-preview/pr-37/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/https://intro-neuro-ai-website-pr-preview.onrender.com/pr-preview/pr-37/assets/js/search-data.json', true);
+  request.open('GET', '/pr-preview/pr-37/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -432,7 +432,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/https://intro-neuro-ai-website-pr-preview.onrender.com/pr-preview/pr-37/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/pr-preview/pr-37/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Scroll site-nav to ensure the link to the current page is visible
